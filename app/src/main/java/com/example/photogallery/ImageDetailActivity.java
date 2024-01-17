b34fae2299e1a9b421378dc4d677d4e5ae87e1c6
Arkadiusz Mroczkowski <80561@student.pb.edu.pl>
2024-01-17 03:08:14 +0100

    Random Quote API

--- app/src/main/java/com/example/photogallery/ImageDetailActivity.java
+++ app/src/main/java/com/example/photogallery/ImageDetailActivity.java
@@ -1,5 +1,6 @@
 package com.example.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -8,9 +9,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.squareup.picasso.Picasso;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class ImageDetailActivity extends AppCompatActivity {
     private ImageView imageViewDetail;
-    TextView fileName;
+    private TextView fileName;
+    private TextView textQuote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,14 +30,15 @@ public class ImageDetailActivity extends AppCompatActivity {
 
         imageViewDetail = findViewById(R.id.image_view_detail);
         fileName = findViewById(R.id.text_view_name);
+        textQuote = findViewById(R.id.text_view_quote);
+        fetchRandomQuote();
 
-        // Retrieve the photo URL from the Intent extras
         String photoUrl = getIntent().getStringExtra("photo_url");
         String photoName = getIntent().getStringExtra("photo_name");
 
         fileName.setText(photoName);
 
-        // Load and display the selected photo using Picasso or any other image-loading library
+
         Picasso.with(this)
                 .load(photoUrl)
                 .placeholder(R.mipmap.ic_launcher)
@@ -33,6 +46,50 @@ public class ImageDetailActivity extends AppCompatActivity {
                 .centerCrop()
                 .into(imageViewDetail);
 
-        // Add code to display other photo details as needed
+    }
+
+
+    private void fetchRandomQuote() {
+        new AsyncTask<Void, Void, Quote>() {
+            @Override
+            protected Quote doInBackground(Void... voids) {
+                try {
+                    URL url = new URL("https://api.quotable.io/random");
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    InputStream inputStream = connection.getInputStream();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    return parseQuote(response.toString());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Quote quote) {
+                super.onPostExecute(quote);
+                if (quote != null) {
+                    textQuote.setText("\"" + quote.getContent() + "\"\n- " + quote.getAuthor());
+                }
+            }
+        }.execute();
+    }
+
+    private Quote parseQuote(String json) {
+        try {
+            JSONObject jsonObject = new JSONObject(json);
+            String content = jsonObject.getString("content");
+            String author = jsonObject.getString("author");
+            return new Quote(content, author);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 }
