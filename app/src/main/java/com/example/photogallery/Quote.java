7d39c25ca7ee87b3b37718be891124573e2e8baa
Arkadiusz Mroczkowski <80561@student.pb.edu.pl>
2024-01-18 12:39:52 +0100

    Location fetching and displaying in details view

---
+++ app/src/main/java/com/example/photogallery/Quote.java
@@ -0,0 +1,20 @@
+package com.example.photogallery;
+
+public class Quote {
+    private String content;
+    private String author;
+
+    public Quote(String content, String author) {
+        this.content = content;
+        this.author = author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+}
